44068d36d3c411a729a76e33d031837616a59cb6
arttkach-ua <art.tkach@gmail.com>
2022-05-08 23:06:39 +0300

    added SQL script for first insert added TransmissionTypes class(needs to continue)

---
+++ src/main/SQL/inserts.sql
@@ -0,0 +1,16 @@
+/*
+* scripts to adding fuel types in database
+*/
+insert into fuel_types(fuel_type_ua, fuel_type_en) value ('Дизель','DIESEl');
+insert into fuel_types(fuel_type_ua, fuel_type_en) value ('Бензин','BENZINE');
+insert into fuel_types(fuel_type_ua, fuel_type_en) value ('Гібрид','HYBRID');
+insert into fuel_types(fuel_type_ua, fuel_type_en) value ('Електрика','ELECTRO');
+/*
+* scripts to adding transmission types in database
+*/
+
+insert into transmission_types(transmission_type_en, transmission_type_ua) value ('Automatic','Автомат');
+insert into transmission_types(transmission_type_en, transmission_type_ua) value ('Manual','Механіка');
+/*
+* scripts to adding body_styles
+*/
--- src/main/java/model/entity/Car.java
+++ src/main/java/model/entity/Car.java
@@ -2,6 +2,7 @@ package model.entity;
 
 import model.entity.enums.CarClasses;
 import model.entity.enums.FuelTypes;
+import model.entity.enums.TransmissionTypes;
 
 public class Car extends Entity{
     CarBrand brand;
@@ -9,6 +10,6 @@ public class Car extends Entity{
     CarClasses carClass;
     int graduationYear;
     String bodyStyle;//!!!!!
-    String transmission;
+    TransmissionTypes transmission;
     FuelTypes fuelType;
 }
