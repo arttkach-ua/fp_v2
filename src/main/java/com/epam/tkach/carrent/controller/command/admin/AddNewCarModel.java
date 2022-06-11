f26d6ecf476052e7f70272014443a55ad383d461
arttkach-ua <art.tkach@gmail.com>
2022-06-11 13:38:38 +0300

    refactor to services and prg

--- src/main/java/com/epam/tkach/carrent/controller/command/admin/AddNewCarModel.java
+++ src/main/java/com/epam/tkach/carrent/controller/command/admin/AddNewCarModel.java
@@ -3,57 +3,35 @@ package com.epam.tkach.carrent.controller.command.admin;
 import com.epam.tkach.carrent.controller.Messages;
 import com.epam.tkach.carrent.controller.PageParameters;
 import com.epam.tkach.carrent.controller.Path;
+import com.epam.tkach.carrent.controller.RequestReader;
 import com.epam.tkach.carrent.controller.command.ICommand;
-import com.epam.tkach.carrent.controller.exceptions.CarBrandRepoException;
 import com.epam.tkach.carrent.controller.exceptions.CarModelRepoException;
-import com.epam.tkach.carrent.model.entity.CarBrand;
-import com.epam.tkach.carrent.model.entity.CarModel;
-import com.epam.tkach.carrent.model.entity.enums.CarClass;
-import com.epam.tkach.carrent.model.repository.CarBrandRepoI;
-import com.epam.tkach.carrent.model.repository.CarModelRepoI;
-import com.epam.tkach.carrent.model.repository.MySqlImp.CarBrandRepoMySql;
-import com.epam.tkach.carrent.model.repository.MySqlImp.CarModelRepoMySql;
+import com.epam.tkach.carrent.model.service.CarModelService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Optional;
 
 public class AddNewCarModel implements ICommand {
     private static final Logger logger = LogManager.getLogger(AddNewCarModel.class);
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        ArrayList<String> errorList = new ArrayList();
-        String classParam = request.getParameter(PageParameters.CAR_CLASS);
-        String brandIdParam = request.getParameter(PageParameters.CAR_BRAND);
-        String modelName = request.getParameter(PageParameters.NAME);
+        int classId = RequestReader.readIntFromRequest(request, PageParameters.CAR_CLASS);
+        int brandId = RequestReader.readIntFromRequest(request, PageParameters.CAR_BRAND);
+        String modelName = RequestReader.readStringFromRequest(request, PageParameters.NAME);
         //Checking for empty model field
         if (modelName.isEmpty()){
-            errorList.add(Messages.ERROR_NULL_CAR_MODEL_NAME);
-            request.setAttribute(PageParameters.ERRORS, errorList);
-            return Path.PAGE_ERROR_PAGE;
+            return Path.prepareErrorPage(request, response, Messages.ERROR_NULL_CAR_MODEL_NAME);
         }
 
-        boolean success = false;
-
-        CarModelRepoI modelRepo = new CarModelRepoMySql();
-
-        CarClass carClass = CarClass.getByID(Integer.parseInt(classParam));
         try {
-            success = modelRepo.addNew(Integer.parseInt(brandIdParam),Integer.parseInt(classParam),modelName);
+            CarModelService.addNew(brandId,modelName,classId);
+            return Path.prepareSuccessPage(request, response, null);
         } catch (CarModelRepoException ex) {
-            errorList.add(Messages.ERROR_DATABASE_ERROR);
             logger.error(ex);
-        }
-
-        if (success){
-            return Path.PAGE_SUCCESS;
-        }else{
-            request.setAttribute(PageParameters.ERRORS, errorList);
-            return Path.PAGE_ERROR_PAGE;
+            return Path.prepareErrorPage(request, response, Messages.ERROR_DATABASE_ERROR);
         }
     }
 }
