2a8e1010e3e62265a6f8978a8ba2681c5755c889
arttkach-ua <art.tkach@gmail.com>
2022-06-11 12:10:28 +0300

    refactor to services and prg

--- src/main/java/com/epam/tkach/carrent/controller/command/common/UpdateProfile.java
+++ src/main/java/com/epam/tkach/carrent/controller/command/common/UpdateProfile.java
@@ -7,45 +7,37 @@ import com.epam.tkach.carrent.controller.RequestReader;
 import com.epam.tkach.carrent.controller.command.ICommand;
 import com.epam.tkach.carrent.controller.exceptions.UserRepoException;
 import com.epam.tkach.carrent.model.entity.User;
-import com.epam.tkach.carrent.model.repository.MySqlImp.UserRepoMySql;
-import com.epam.tkach.carrent.model.repository.UserRepoI;
+import com.epam.tkach.carrent.model.service.UserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.Optional;
 
 public class UpdateProfile implements ICommand {
     private static final Logger logger = LogManager.getLogger(UpdateProfile.class);
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession(false);
-        UserRepoI repo = new UserRepoMySql();
 
         if (session == null) {
             logger.debug("Session error");
-            return Path.prepareErrorPage(request, Messages.ERROR_DATABASE_ERROR);
+            return Path.prepareErrorPage(request,response, Messages.ERROR_SESSION_ERROR);
         }
         try {
             int id = (int)session.getAttribute(PageParameters.ID);
-            Optional<User> userOpt = repo.findByID(id);
-            if (userOpt.isEmpty()){
-                logger.debug("Didnt get user from db, id=" + id);
-                return Path.prepareErrorPage(request, Messages.ERROR_DATABASE_ERROR);
-            }
-            User user = userOpt.get();
+
+            User user = UserService.findByID(id);
             updateUserfromRequest(request, user);
             logger.debug(user.toString());
-            repo.update(user);
-            return Path.PAGE_SUCCESS;
+            UserService.update(user);
+            return Path.prepareSuccessPage(request, response, null);
 
         } catch (UserRepoException e) {
             logger.error(e);
+            return Path.prepareErrorPage(request, response, Messages.ERROR_DATABASE_ERROR);
         }
-
-        return null;
     }
 
     private void updateUserfromRequest(HttpServletRequest request, User user){
