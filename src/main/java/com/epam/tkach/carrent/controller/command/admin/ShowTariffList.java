a3644a3bb2e11927746656363dd166b2c29db2ab
arttkach-ua <art.tkach@gmail.com>
2022-06-11 14:03:21 +0300

    refactor to services and prg

--- src/main/java/com/epam/tkach/carrent/controller/command/admin/ShowTariffList.java
+++ src/main/java/com/epam/tkach/carrent/controller/command/admin/ShowTariffList.java
@@ -5,18 +5,9 @@ import com.epam.tkach.carrent.controller.PageParameters;
 import com.epam.tkach.carrent.controller.PaginationHelper;
 import com.epam.tkach.carrent.controller.Path;
 import com.epam.tkach.carrent.controller.command.ICommand;
-import com.epam.tkach.carrent.controller.exceptions.CarModelRepoException;
-import com.epam.tkach.carrent.controller.exceptions.CompleteSetsRepoException;
 import com.epam.tkach.carrent.controller.exceptions.TariffException;
-import com.epam.tkach.carrent.model.entity.CarModel;
-import com.epam.tkach.carrent.model.entity.CompleteSet;
 import com.epam.tkach.carrent.model.entity.Tariff;
-import com.epam.tkach.carrent.model.repository.CarModelRepoI;
-import com.epam.tkach.carrent.model.repository.MySqlImp.CarModelRepoMySql;
-import com.epam.tkach.carrent.model.repository.MySqlImp.TariffRepoMySql;
-import com.epam.tkach.carrent.model.repository.RepositoryFactory;
-import com.epam.tkach.carrent.model.repository.TariffRepoI;
-import com.epam.tkach.carrent.model.service.CompleteSetService;
+import com.epam.tkach.carrent.model.service.TariffService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -30,13 +21,11 @@ public class ShowTariffList implements ICommand {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         try {
-            TariffRepoI repo = RepositoryFactory.getTariffRepo();
-            int countInDB = repo.getCountInDB();
+            int countInDB = TariffService.getCountInDB();
             int currentPage = PaginationHelper.getCurrentPage(request);
             int recordsPerPage = 5;
 
-
-            List<Tariff> listOfTariffs = repo.getListForPagination(currentPage, recordsPerPage);
+            List<Tariff> listOfTariffs = TariffService.getListForPagination(currentPage, recordsPerPage);
             int nOfPages = PaginationHelper.getNoOfPages(countInDB,recordsPerPage);
 
             request.setAttribute(PageParameters.TARIFF_LIST, listOfTariffs);
@@ -46,7 +35,7 @@ public class ShowTariffList implements ICommand {
             return Path.PAGE_ALL_TARIFFS;
         } catch (TariffException ex) {
             logger.error(ex);
-            return Path.prepareErrorPage(request, Messages.ERROR_DATABASE_ERROR);
+            return Path.prepareErrorPage(request,response, Messages.ERROR_DATABASE_ERROR);
         }
     }
 }
