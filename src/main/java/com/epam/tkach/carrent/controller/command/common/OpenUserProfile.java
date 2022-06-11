924d52ba47917365cddabab6c2c9dafe0fc2f753
arttkach-ua <art.tkach@gmail.com>
2022-06-11 12:11:38 +0300

    refactor to services and prg

--- src/main/java/com/epam/tkach/carrent/controller/command/common/OpenUserProfile.java
+++ src/main/java/com/epam/tkach/carrent/controller/command/common/OpenUserProfile.java
@@ -6,43 +6,34 @@ import com.epam.tkach.carrent.controller.Path;
 import com.epam.tkach.carrent.controller.command.ICommand;
 import com.epam.tkach.carrent.controller.exceptions.UserRepoException;
 import com.epam.tkach.carrent.model.entity.User;
-import com.epam.tkach.carrent.model.repository.MySqlImp.UserRepoMySql;
-import com.epam.tkach.carrent.model.repository.UserRepoI;
+import com.epam.tkach.carrent.model.service.UserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.Optional;
 
 public class OpenUserProfile implements ICommand {
     private static final Logger logger = LogManager.getLogger(OpenUserProfile.class);
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession(false);
-        UserRepoI repo = new UserRepoMySql();
 
         if (session == null) {
             logger.debug("Session error");
-            return Path.prepareErrorPage(request, Messages.ERROR_DATABASE_ERROR);
+            return Path.prepareErrorPage(request,response, Messages.ERROR_DATABASE_ERROR);
         }
         try {
             int id = (int)session.getAttribute(PageParameters.ID);
-            Optional<User> userOpt = repo.findByID(id);
-            if (userOpt.isEmpty()){
-                logger.debug("Didnt get user from db, id=" + id);
-                return Path.prepareErrorPage(request, Messages.ERROR_DATABASE_ERROR);
-            }
-            User user = userOpt.get();
+            User user = UserService.findByID(id);
             setUserPropertiesToRequest(request, user);
             return Path.PAGE_MY_PROGILE;
 
         } catch (UserRepoException e) {
             logger.error(e);
+            return Path.prepareErrorPage(request,response, Messages.ERROR_DATABASE_ERROR);
         }
-
-        return null;
     }
 
     private void setUserPropertiesToRequest(HttpServletRequest request, User user){
