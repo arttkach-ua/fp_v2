93843d6fa3408ed15b22998cddd9e30954301fe5
arttkach-ua <art.tkach@gmail.com>
2022-06-11 12:31:02 +0300

    refactor to services and prg

--- src/main/java/com/epam/tkach/carrent/controller/Controller.java
+++ src/main/java/com/epam/tkach/carrent/controller/Controller.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.web.WebLoggerContextUtils;
 
 /**
  * Main Controller Servlet.
