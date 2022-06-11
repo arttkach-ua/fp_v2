84b500f7c1558476564fd8ab632acc0dcfa55413
arttkach-ua <art.tkach@gmail.com>
2022-06-11 13:39:08 +0300

    refactor to services and prg

--- src/main/java/com/epam/tkach/carrent/controller/command/admin/ShowCarList.java
+++ src/main/java/com/epam/tkach/carrent/controller/command/admin/ShowCarList.java
@@ -1,20 +1,20 @@
 package com.epam.tkach.carrent.controller.command.admin;
 
-import com.epam.tkach.carrent.controller.Messages;
-import com.epam.tkach.carrent.controller.PageParameters;
-import com.epam.tkach.carrent.controller.PaginationHelper;
-import com.epam.tkach.carrent.controller.Path;
+import com.epam.tkach.carrent.controller.*;
 import com.epam.tkach.carrent.controller.command.ICommand;
+import com.epam.tkach.carrent.controller.exceptions.CarBrandRepoException;
 import com.epam.tkach.carrent.controller.exceptions.CarRepoException;
 import com.epam.tkach.carrent.model.entity.Car;
-import com.epam.tkach.carrent.model.repository.CarRepoI;
-import com.epam.tkach.carrent.model.repository.MySqlImp.CarRepoMySql;
+import com.epam.tkach.carrent.model.entity.enums.CarClass;
+import com.epam.tkach.carrent.model.entity.enums.TransmissionTypes;
+import com.epam.tkach.carrent.model.service.CarBrandService;
+import com.epam.tkach.carrent.model.service.CarService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class ShowCarList implements ICommand {
@@ -22,22 +22,44 @@ public class ShowCarList implements ICommand {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         try {
-            CarRepoI repo = new CarRepoMySql();
-            int carBrandsCount = repo.getCountInDB();
+            int carClassFilter =  RequestReader.readIntFromRequest(request, PageParameters.CAR_CLASS_FILTER);
+            int carBrandFilter = RequestReader.readIntFromRequest(request, PageParameters.CAR_BRAND_FILTER);
+            int transmissionFilter = RequestReader.readIntFromRequest(request, PageParameters.TRANSMISSION_FILTER);
+            String sortBy = RequestReader.readStringFromRequest(request, PageParameters.SORT_FIELD);
+
+            LinkedHashMap<String, Object> filters = new LinkedHashMap<>();
+            filters.put("cars.available", true);
+            if (carClassFilter != -1&&carClassFilter != 0){
+                filters.put("cm.car_classes_id", carClassFilter);
+                request.setAttribute(PageParameters.CAR_CLASS_FILTER, carClassFilter);
+            }
+            if (carBrandFilter != -1&&carBrandFilter != 0){
+                filters.put("cars.brand_id", carBrandFilter);
+                request.setAttribute(PageParameters.CAR_BRAND_FILTER, carBrandFilter);
+            }
+            if (transmissionFilter != -1&&transmissionFilter != 0){
+                filters.put("cs.transmission_id", transmissionFilter);
+                request.setAttribute(PageParameters.TRANSMISSION_FILTER, transmissionFilter);
+            }
+            int carBrandsCount = CarService.getCountInDB(filters);
             int currentPage = PaginationHelper.getCurrentPage(request);
             int recordsPerPage = 5;
 
-            List<Car> carList = repo.getListForPagination(currentPage, recordsPerPage);
+            List<Car> carList = CarService.getList(currentPage, recordsPerPage, filters, CarService.getColumnNameToSort(sortBy));
             int nOfPages = PaginationHelper.getNoOfPages(carBrandsCount, recordsPerPage);
-
+            request.setAttribute(PageParameters.SORT_FIELD, sortBy);
+            request.setAttribute(PageParameters.SORT_LIST, CarService.getSortingList());
+            request.setAttribute(PageParameters.CAR_BRAND_LIST, CarBrandService.getAll());
+            request.setAttribute(PageParameters.TRANSMISSION_LIST, TransmissionTypes.values());
+            request.setAttribute(PageParameters.CAR_CLASS_LIST, CarClass.values());
             request.setAttribute(PageParameters.CAR_LIST, carList);
             request.setAttribute(PageParameters.NO_OF_PAGES, nOfPages);
             request.setAttribute(PageParameters.CURRENT_PAGE, currentPage);
             request.setAttribute(PageParameters.RECORD_PER_PAGE, recordsPerPage);
             return Path.PAGE_ALL_CARS;
-        } catch (CarRepoException e) {
+        } catch (CarRepoException | CarBrandRepoException e) {
             logger.error(e);
-            return Path.prepareErrorPage(request, Messages.ERROR_DATABASE_ERROR);
+            return Path.prepareErrorPage(request, response, Messages.ERROR_DATABASE_ERROR);
         }
     }
 }
