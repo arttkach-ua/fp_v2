2ce5130e481b25852074c2db4e02f0e3411a58f6
arttkach-ua <art.tkach@gmail.com>
2022-06-11 12:52:09 +0300

    refactor to services and prg

--- src/main/java/com/epam/tkach/carrent/controller/command/admin/SetUserBlock.java
+++ src/main/java/com/epam/tkach/carrent/controller/command/admin/SetUserBlock.java
@@ -6,15 +6,13 @@ import com.epam.tkach.carrent.controller.Path;
 import com.epam.tkach.carrent.controller.RequestReader;
 import com.epam.tkach.carrent.controller.command.ICommand;
 import com.epam.tkach.carrent.controller.exceptions.UserRepoException;
-import com.epam.tkach.carrent.model.entity.User;
-import com.epam.tkach.carrent.model.repository.MySqlImp.UserRepoMySql;
-import com.epam.tkach.carrent.model.repository.UserRepoI;
+import com.epam.tkach.carrent.model.service.UserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Optional;
+
 
 public class SetUserBlock implements ICommand {
     private static final Logger logger = LogManager.getLogger(SetUserBlock.class);
@@ -24,23 +22,12 @@ public class SetUserBlock implements ICommand {
         int id = RequestReader.readIntFromRequest(request, PageParameters.ID);
         String param = RequestReader.readStringFromRequest(request, PageParameters.NEW_VALUE);
         boolean newValue = "true".equals(param);
-
-        UserRepoI repo = new UserRepoMySql();
-
         try {
-            Optional<User> userOpt = repo.findByID(id);
-            if (userOpt.isEmpty()){
-                logger.error("Error in SetUserBlock method. User with id".concat(Integer.toString(id)).concat(" not found."));
-                return Path.prepareErrorPage(request, Messages.ERROR_DATABASE_ERROR);
-            }
-            User user = userOpt.get();
-            user.setBlocked(newValue);
-            logger.debug("new Value:::".concat(Boolean.toString(newValue)));
-            repo.update(user);
-            return Path.PAGE_SUCCESS;
+            UserService.setBlockToUser(id, newValue);
+            return Path.prepareSuccessPage(request, response, null);
         } catch (UserRepoException e) {
             logger.error(e);
-            return Path.prepareErrorPage(request, Messages.ERROR_DATABASE_ERROR);
+            return Path.prepareErrorPage(request,response, Messages.ERROR_DATABASE_ERROR);
         }
     }
 }
