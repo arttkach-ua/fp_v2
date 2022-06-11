734b9d40f2a22e9fb5b0da9f029c29735274113f
arttkach-ua <art.tkach@gmail.com>
2022-06-11 12:41:43 +0300

    refactor to services and prg

--- src/main/java/com/epam/tkach/carrent/controller/command/admin/OpenEditTariffPage.java
+++ src/main/java/com/epam/tkach/carrent/controller/command/admin/OpenEditTariffPage.java
@@ -6,14 +6,12 @@ import com.epam.tkach.carrent.controller.Path;
 import com.epam.tkach.carrent.controller.RequestReader;
 import com.epam.tkach.carrent.controller.command.ICommand;
 import com.epam.tkach.carrent.controller.exceptions.TariffException;
-import com.epam.tkach.carrent.model.entity.Tariff;
 import com.epam.tkach.carrent.model.service.TariffService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.security.MessageDigest;
 
 public class OpenEditTariffPage implements ICommand {
     private static final Logger logger = LogManager.getLogger(OpenEditTariffPage.class);
@@ -27,7 +25,7 @@ public class OpenEditTariffPage implements ICommand {
             return Path.PAGE_EDIT_TARIFF;
         } catch (TariffException e) {
             logger.error(e);
-            return Path.prepareErrorPage(request, Messages.ERROR_DATABASE_ERROR);
+            return Path.prepareErrorPage(request,response, Messages.ERROR_DATABASE_ERROR);
         }
     }
 }
